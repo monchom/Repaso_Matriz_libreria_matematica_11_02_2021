fc27150c30f78d72632fe180532569e0fa6860a8
Adrian velasquez Guerrero <adrian.velasquez.guerrero@gmail.com>
2021-02-11 20:26:11 -0500

    Repaso matrices y libreria matematicas

---
+++ main.js
@@ -0,0 +1,233 @@
+
+
+
+
+
+
+
+// let array = new Array(5).fill(()=>{return `USUARIO NO ENVIO NADA`;});
+
+// array.fill("Adrian",1,4);
+
+// let id = 3;
+
+// if(typeof(array[id])!="function"){
+//     console.log(array[id]);
+// }else{
+//     console.log(array[id]());
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// declarar matrices perfectas
+
+// const cont = 10;
+// const array = new Array(cont);
+// for (let i = 0; i < cont; i++) {
+//     array[i] = new Array(cont).fill(null);
+// }
+
+// array[0][0] = "Adrian";
+// array[1][1] = "Velasquez";
+// array[2][2] = 50;
+// array[3][3] = true;
+// array[3][3] = "Calle 102c B".toUpperCase();
+// console.log(array);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// matriz construida manual mente
+
+// const matriz = [
+//     "Miguel",
+//     23,
+//     true,
+//     ()=>{
+//         let data = 2 + 2;
+//         return `${data}`;
+//     },
+//     8%2,
+//     [
+//         [
+//             "Txllagts",
+//             "andrea"
+//         ]
+//     ]
+// ];
+
+// console.log(matriz[5][0][1]);
+
+
+//Librearia matematica
+
+// let raiz_cuadrada = Math.sqrt(5);
+// console.log(raiz_cuadrada);
+
+// let raiz_cubica = Math.cbrt(27);
+// console.log(raiz_cubica);
+
+// let data = [1,5,8,100,500,5,9];
+// console.log(parseInt(data.join(",")));
+// let numero_mayor = Math.max(data.join(","));
+
+
+
+
+
+
+// console.log(Math.pow(5, 2));
+
+
+
+// Libreria matematica
+// let raiz_cuadrada = Math.sqrt(25);
+// console.log(raiz_cuadrada);
+
+// let raiz_cubica = Math.cbrt(27);
+// console.log(raiz_cubica);
+
+
+// let numero_mayor = Math.max(1,5,8,100,500,5,9);
+// console.log(numero_mayor);
+
+// let numero_menor = Math.min(9,5,8,100,500,5,9);
+// console.log(numero_menor);
+
+
+
+// for (let i = 0; i < 10; i++) {
+//     let aleatorio = Math.random()*100;
+//     document.write(Math.trunc(aleatorio)+"<br>");
+    
+// }
+
+// let aleatorio = Math.random()*100;
+// document.write(" Numero aleatorio <br>"+aleatorio.toString());
+// document.write("<br> Redondear despues de 5<br>"+Math.round(aleatorio));
+// document.write("<br> Redondear antesde de 5<br>"+Math.floor(aleatorio));
+// document.write("<br> Tomar el numero entero<br>"+Math.trunc(aleatorio));
+
+
+// console.log(Math.PI);
+// console.log(Math.SQRT1_2);
+// console.log(Math.SQRT2);
+// console.log(Math.E);
+// console.log(Math.LN2);
+// console.log(Math.LN10);
+// console.log(Math.LOG2E);
+// console.log(Math.LOG10E);
+
+
+
+
+
+
+// Metodo de relleno con lista
+
+// let array = new Array(5).fill(()=>{return `USUARIO NO ENVIO NADA`;});
+
+// array.fill("adrian",1,4);
+
+// let id = 3;
+
+// if(typeof(array[id])!="function"){
+//     console.log(array[id]);
+// }else{
+//     console.log(array[id]());
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// declarar matrices perfectas
+
+// const cont = 10;
+// const array = new Array(cont);
+// for (let i = 0; i < cont; i++) {
+//     array[i] = new Array(cont).fill(null);
+// }
+
+// array[0][0] = "Adrian";
+// array[1][1] = "Velasquez";
+// array[2][2] = 50;
+// array[3][3] = true;
+// array[3][3] = "Calle 11 B".toUpperCase();
+// console.log(array);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// matriz construida manual mente
+
+// const matriz = [
+//     "Adrian Velasquez",
+//     50,
+//     true,
+//     ()=>{
+//         let data = 2 + 2;
+//         return `${data}`;
+//     },
+//     8%2,
+//     [
+//         [
+//             "Txllagts",
+//             "andrea"
+//         ]
+//     ]
+// ];
+
+// console.log(matriz[5][0][1]);
+
+
